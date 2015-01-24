92fb2462173ddd08aa254eeffd8ff1d03ac511a3
Alexander Smirnov <alexander.smirnov@postindustria.com>
2015-01-25 02:41:02 +0500

    firstcommit

---
+++ app/src/androidTest/java/ru/smirnov/test/instagram/photocollage/ApplicationTest.java
@@ -0,0 +1,13 @@
+package ru.smirnov.test.instagram.photocollage;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
